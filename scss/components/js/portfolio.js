0cfcfb8da163b5ef26d4278801ec3fbef6aae618
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-06-17 16:59:35 +0200

    added javascript logic to portfolio buttons, so we can cycle through different projects based on category

--- scss/components/js/portfolio.js
+++ scss/components/js/portfolio.js
@@ -1,1 +1,23 @@
 // selectors
+const all = document.querySelector("#all");
+const websites = document.querySelector("#websites");
+const apps = document.querySelector("#apps");
+const components = document.querySelector("#components");
+
+const allItems = document.querySelectorAll(".p-item");
+
+const displayItems = function (e) {
+  if (e.target.id === `websites` || `apps` || `components` || `all`)
+    allItems.forEach((item) => {
+      if (!item.classList.contains(e.target.id)) {
+        item.classList.add(`hide`);
+      } else {
+        item.classList.remove(`hide`);
+      }
+    });
+};
+
+websites.addEventListener("click", displayItems);
+apps.addEventListener("click", displayItems);
+all.addEventListener("click", displayItems);
+components.addEventListener("click", displayItems);
