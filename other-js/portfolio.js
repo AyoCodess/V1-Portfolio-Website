f49b5977a136c7cbf0c2a43409da3e4f55f995fb
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-06-18 16:37:39 +0200

    fixed buttons designed in portfilio section, added hide all button also with JS

--- other-js/portfolio.js
+++ other-js/portfolio.js
@@ -3,10 +3,12 @@ const all = document.querySelector("#all");
 const websites = document.querySelector("#websites");
 const apps = document.querySelector("#apps");
 const components = document.querySelector("#components");
+const hideAll = document.querySelector("#hide");
 
 const allItems = document.querySelectorAll(".p-item");
 
 const displayItems = function (e) {
+  console.log(e.target.id);
   if (e.target.id === `websites` || `apps` || `components` || `all`)
     allItems.forEach((item) => {
       if (!item.classList.contains(e.target.id)) {
@@ -15,9 +17,17 @@ const displayItems = function (e) {
         item.classList.remove(`hide`);
       }
     });
+
+  if (e.target.id === "hide") {
+    console.log(e.target.id);
+    allItems.forEach((item) => {
+      item.classList.add("hide");
+    });
+  }
 };
 
 websites.addEventListener("click", displayItems);
 apps.addEventListener("click", displayItems);
 all.addEventListener("click", displayItems);
 components.addEventListener("click", displayItems);
+hideAll.addEventListener("click", displayItems);
