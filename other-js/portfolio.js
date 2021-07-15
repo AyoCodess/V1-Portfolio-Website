40794c343e94d3b6252c7c9f6413018334efe57f
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-07-15 12:44:18 +0200

    portfolio bug fix

--- other-js/portfolio.js
+++ other-js/portfolio.js
@@ -5,13 +5,13 @@ const apps = document.querySelector("#apps");
 const components = document.querySelector("#components");
 const hideAll = document.querySelector("#hide");
 const portfolioContainer = document.querySelector(`.portfolio-container`);
-const scrollText = document.querySelector(`.scroll-text`);
+// const scrollText = document.querySelector(`.scroll-text`);
 
 const allItems = document.querySelectorAll(".p-item");
 
 const loader = document.querySelectorAll(`.loader`);
 
-const mediaQuery = window.matchMedia("(max-width: 569px)");
+// const mediaQuery = window.matchMedia("(max-width: 569px)");
 
 //?  image loader screen
 window.addEventListener("load", function () {
@@ -26,35 +26,26 @@ const displayItems = function (e) {
       if (!item.classList.contains(e.target.id)) {
         item.classList.add(`hide`);
         portfolioContainer.hidden = false;
-        if (mediaQuery.matches && !scrollText.classList.contains(`hide`)) {
-          scrollText.classList.remove("hide");
-        }
+        // if (mediaQuery.matches && !scrollText.classList.contains(`hide`)) {
+        //   scrollText.classList.remove("hide");
+        // }
       } else {
         item.classList.remove(`hide`);
         portfolioContainer.hidden = false;
 
-        if (mediaQuery.matches && scrollText.classList.contains(`hide`)) {
-          scrollText.classList.remove("hide");
-        }
+        // if (mediaQuery.matches && scrollText.classList.contains(`hide`)) {
+        //   scrollText.classList.remove("hide");
+        // }
       }
     });
-
-  // //? Scroll text hide
-  // if (mediaQuery.matches && !scrollText.classList.contains(`hide`)) {
-  //   scrollText.classList.add("hide");
-  // } else {
-  //   if (mediaQuery.matches && scrollText.classList.contains(`hide`)) {
-  //     scrollText.classList.remove("hide");
-  //   }
-  // }
 
   if (e.target.id === "hide") {
     allItems.forEach((item) => {
       item.classList.add("hide");
       portfolioContainer.hidden = true;
-      if (mediaQuery.matches && !scrollText.classList.contains(`hide`)) {
-        scrollText.classList.add("hide");
-      }
+      // if (mediaQuery.matches && !scrollText.classList.contains(`hide`)) {
+      //   scrollText.classList.add("hide");
+      // }
     });
   }
 };
@@ -64,4 +55,4 @@ apps.addEventListener("click", displayItems);
 all.addEventListener("click", displayItems);
 components.addEventListener("click", displayItems);
 hideAll.addEventListener("click", displayItems);
-window.addEventListener("resize", scrollTextHide);
+// window.addEventListener("resize", scrollTextHide);
