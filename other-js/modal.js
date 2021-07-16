f62c15b8bda5864e15021d070d701812bc4b6ce7
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-07-16 03:03:27 +0200

    added coding blog to portfolio showcase

--- other-js/modal.js
+++ other-js/modal.js
@@ -103,6 +103,15 @@ const projectContent = {
     liveDemo: "https://bgchanger-ayo.netlify.app/",
     repo: "https://github.com/AyoCodess/background-changer-app",
   },
+  ayoBlogSite: {
+    name: "Ayo Codes - The Blog",
+    description:
+      " This was my first time using a static site generator with a headless CMS. We have pagination functionality for every 10 posts. It was challenging at times but I got the job done. Visit the blog to read more about my coding ups and downs.",
+    technology:
+      "<strong>HTML | SASS | JS | 11ty | Nunjunks | Netlify CMS | Disqus</strong>",
+    liveDemo: "https://blog.ayoadesanya.com",
+    repo: "https://github.com/AyoCodess/my-coding-blog",
+  },
   dbzMusicPlayer: {
     name: "DBZ Music Player",
     description:
