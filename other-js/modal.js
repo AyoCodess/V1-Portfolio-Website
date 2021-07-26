77951e85e566dea379878badb4e600bdba0c7d13
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-07-26 15:07:11 +0200

    added questions to faq section

--- other-js/modal.js
+++ other-js/modal.js
@@ -42,7 +42,7 @@ const projectContent = {
     name: "Microsoft Website 2021",
     description:
       "I had good time building the desktop and mobile versions, emulating the original website as best I could with a few new additions.",
-    technology: "<strong>HTML | CSS | JS</strong>",
+    technology: "<b>HTML | CSS | JS</b>",
     liveDemo: "https://microsoft-2021-website-ayocodess.netlify.app/",
     repo: "https://github.com/AyoCodess/Microsoft-Website-2021",
   },
@@ -50,7 +50,7 @@ const projectContent = {
     name: "Luxury Real Estate Company",
     description:
       "One of my early projects. I wanted to give off a luxury feel... did I succeed? ",
-    technology: "<strong>HTML | CSS GRID ONLY</strong>",
+    technology: "<b>HTML | CSS GRID ONLY</b>",
     liveDemo: "https://realtor-landing-page-css-grid.netlify.app/",
     repo: "https://github.com/AyoCodess/CSS-Grid-Landing-Page",
   },
@@ -58,7 +58,7 @@ const projectContent = {
     name: "Isit A Good Investment?",
     description:
       "Learn how to invest in crypto safely and join a private syndicate of investors - Landing page.",
-    technology: "<strong>HTML | SASS | JS | AOS Library </strong>",
+    technology: "<b>HTML | SASS | JS | AOS Library </b>",
     liveDemo: "https://isitagoodinvestment.netlify.app/",
     repo: "https://github.com/AyoCodess/IIAGI-Website",
   },
@@ -66,14 +66,14 @@ const projectContent = {
     name: "SASS Website - Ultra Light/Dark Mode 2021",
     description:
       "Re-envisioned the SASS landing page with a dark and light theme implementation. Local storage automatically saves your choice of theme.",
-    technology: "<strong>HTML | SASS | JS</strong>",
+    technology: "<b>HTML | SASS | JS</b>",
     liveDemo: "https://sass-landing-page-ayocodess.netlify.app/",
     repo: "https://github.com/AyoCodess/Sass-Landing-Page",
   },
   travelCompanyLandingPage: {
     name: "Travel Company Landing Page",
     description: "Simple brochure style page for a travel company. ",
-    technology: "<strong>HTML | CSS </strong>",
+    technology: "<b>HTML | CSS </b>",
     liveDemo: "https://ayo-booking-app.netlify.app/",
     repo: "https://github.com/AyoCodess/trillo-landing-page",
   },
@@ -82,7 +82,7 @@ const projectContent = {
     name: "Netflix Crimson 2021",
     description:
       "Completely re-built from the ground up with a nice customized Bootstrap V.5 FAQ component.  ",
-    technology: "<strong>HTML | CSS | JS | Bootstrap V.5</strong>",
+    technology: "<b>HTML | CSS | JS | Bootstrap V.5</b>",
     liveDemo: "https://crimson-neflix-landpage.netlify.app/",
     repo: "https://github.com/AyoCodess/Crimson-Netflix-Landing-Page",
   },
@@ -90,7 +90,7 @@ const projectContent = {
     name: "This Website!",
     description:
       "This website was built from ground up and is continually being developed. This was also my first attempt and learning and implementing bootstrap for the very first time. ",
-    technology: "<strong>HTML | SASS | JS | Bootstrap V.5 </strong>",
+    technology: "<b>HTML | SASS | JS | Bootstrap V.5 </b>",
     liveDemo: "https://www.ayoadesanya.com/",
     repo: "https://github.com/AyoCodess/Portfolio-Website",
   },
@@ -99,7 +99,7 @@ const projectContent = {
     name: "Background Gradient Generator",
     description:
       "Generate any linear gradient background you want from any two colors you want.",
-    technology: "<strong>HTML | CSS GRID ONLY</strong>",
+    technology: "<b>HTML | CSS GRID ONLY</b>",
     liveDemo: "https://bgchanger-ayo.netlify.app/",
     repo: "https://github.com/AyoCodess/background-changer-app",
   },
@@ -108,7 +108,7 @@ const projectContent = {
     description:
       " This was my first time using a static site generator with a headless CMS. We have pagination functionality for every 10 posts. It was challenging at times but I got the job done. Visit the blog to read more about my coding ups and downs.",
     technology:
-      "<strong>HTML | SASS | JS | 11ty | Nunjunks | Netlify CMS | Disqus</strong>",
+      "<b>HTML | SASS | JS | 11ty | Nunjunks | Netlify CMS | Disqus</b>",
     liveDemo: "https://blog.ayoadesanya.com",
     repo: "https://github.com/AyoCodess/my-coding-blog",
   },
@@ -116,7 +116,7 @@ const projectContent = {
     name: "DBZ Music Player",
     description:
       "Are you a fan of Dragonball Z? Ready for a nostalgic blast from the past? This project is a custom built fully functional DBZ music player hosting my favorite tracks from the series.",
-    technology: "<strong>HTML | CSS | JS </strong>",
+    technology: "<b>HTML | CSS | JS </b>",
     liveDemo:
       "https://js-project-collection.netlify.app/dbz-music-player/dbz-music-player.html",
     repo: "https://github.com/AyoCodess/The-JS-Project-Showcase/tree/master/dbz-music-player",
@@ -125,7 +125,7 @@ const projectContent = {
     name: "Your Countdown",
     description:
       "Create your custom countdown and have it saved to the browser with the help of local storage.",
-    technology: "<strong>HTML | CSS | JS</strong>",
+    technology: "<b>HTML | CSS | JS</b>",
     liveDemo:
       "https://js-project-collection.netlify.app/your-countdown/your-countdown.html",
     repo: "https://github.com/AyoCodess/The-JS-Project-Showcase/tree/master/your-countdown",
@@ -134,8 +134,7 @@ const projectContent = {
     name: "Picture In Picture + Instant Webcam",
     description:
       "Create a floating screen capture and see what you look like via your webcam in an instant!",
-    technology:
-      "<strong>HTML | CSS | JS | Screen Capture API | Webcam API </strong>",
+    technology: "<b>HTML | CSS | JS | Screen Capture API | Webcam API </b>",
     liveDemo:
       "https://js-project-collection.netlify.app/picture-in-picture/picture-in-picture.html",
     repo: "https://github.com/AyoCodess/The-JS-Project-Showcase/tree/master/picture-in-picture",
@@ -145,7 +144,7 @@ const projectContent = {
     name: "Super Navigation",
     description:
       "Animated Navigation that performs and looks great on any website.",
-    technology: "<strong>HTML | CSS | JS </strong>",
+    technology: "<b>HTML | CSS | JS </b>",
     liveDemo:
       "https://js-project-collection.netlify.app/super-navigation/super-navigation.html",
     repo: "https://github.com/AyoCodess/The-JS-Project-Showcase/tree/master/super-navigation",
@@ -153,7 +152,7 @@ const projectContent = {
   jokeTeller: {
     name: "Thw Joker Joke Teller",
     description: "Enter if you dear...",
-    technology: "<strong>HTML | CSS | JS | Text-to-speech API |</strong>",
+    technology: "<b>HTML | CSS | JS | Text-to-speech API |</b>",
     liveDemo:
       "https://js-project-collection.netlify.app/joke-teller/joke-teller.html",
     repo: "https://github.com/AyoCodess/The-JS-Project-Showcase/tree/master/joke-teller",
@@ -162,7 +161,7 @@ const projectContent = {
     name: "Infinite Crypto Scroll",
     description:
       "Using the unsplash API I built this infinite scroll site which can be implemented on any website.",
-    technology: "<strong>HTML | CSS | Unsplash API </strong>",
+    technology: "<b>HTML | CSS | Unsplash API </b>",
     liveDemo:
       "https://js-project-collection.netlify.app/infinite-scroll-gallery/infinite-scroll-gallery",
     repo: "https://github.com/AyoCodess/The-JS-Project-Showcase/tree/master/infinite-scroll-gallery",
@@ -170,7 +169,7 @@ const projectContent = {
   quoteGenerator: {
     name: "Quote Generator",
     description: "Get inspired - Built using a quote API.",
-    technology: "<strong>HTML | CSS | Quote API </strong>",
+    technology: "<b>HTML | CSS | Quote API </b>",
     liveDemo:
       "https://js-project-collection.netlify.app/quote-generator/quote-generator.html",
     repo: "https://js-project-collection.netlify.app/quote-generator/quote-generator.html",
@@ -178,7 +177,7 @@ const projectContent = {
   projectShowcase: {
     name: "JS Project Showcase",
     description: "A collection of javascript projects.",
-    technology: "<strong>HTML | CSS | JS | APIs </strong>",
+    technology: "<b>HTML | CSS | JS | APIs </b>",
     liveDemo: "https://js-project-collection.netlify.app/",
     repo: "https://github.com/AyoCodess/The-JS-Project-Showcase",
   },
@@ -186,14 +185,14 @@ const projectContent = {
   minimalistFrom: {
     name: "The Modern Blue Form",
     description: "A simple, yet modern form.",
-    technology: "<strong>HTML | CSS </strong>",
+    technology: "<b>HTML | CSS </b>",
     liveDemo: "https://modern-blue-form.netlify.app/",
     repo: "https://github.com/AyoCodess/Modern-Blue-Form-Theme",
   },
   basicShoppingListApp: {
     name: "Shopping List App",
     description: "A simple shopping list app.",
-    technology: "<strong>HTML | CSS </strong>",
+    technology: "<b>HTML | CSS </b>",
     liveDemo: "https://the-simple-todo-app.netlify.app/",
     repo: "https://github.com/AyoCodess/The-Simple-todo-App",
   },
