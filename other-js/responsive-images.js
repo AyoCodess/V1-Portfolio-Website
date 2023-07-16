3401bb54ab45fda1ef6cd1623f25aec7bd37e721
AyoCodess <ayo.daniel.adesanya@gmail.com>
2023-07-16 22:02:32 +0200

    added image fn

--- other-js/responsive-images.js
+++ other-js/responsive-images.js
@@ -1,8 +1,15 @@
 const sharp = require("sharp");
 const fs = require("fs");
 const directory = '.public/static/images/arts';
+const directoryProjects = '.public/static/images/key-projects';
 
 fs.readdirSync(directory).forEach((file) => {
+  sharp(`${directory}/${file}`)
+    .resize(200, 100) // width, height
+    .toFile(`${directory}/${file}-small.jpg`);
+});
+
+fs.readdirSync(directoryProjects).forEach((file) => {
   sharp(`${directory}/${file}`)
     .resize(200, 100) // width, height
     .toFile(`${directory}/${file}-small.jpg`);
